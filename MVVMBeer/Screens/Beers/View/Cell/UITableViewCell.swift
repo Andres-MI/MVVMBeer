1e986e8b52f7ce2e8a302ec09adc1c9dea0f160f
Andrés Melenchón <andres.melenchon88@gmail.com>
2022-02-23 21:02:01 +0100

    First commit

---
+++ MVVMBeer/Screens/Beers/View/Cell/UITableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  UITableViewCell.swift
+//  MVVMBeer
+//
+//  Created by Andrés Melenchón on 23/2/22.
+//
+
+import Foundation
---
+++ MVVMBeerUITests/MVVMBeerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVMBeerUITestsLaunchTests.swift
+//  MVVMBeerUITests
+//
+//  Created by Andrés Melenchón on 23/2/22.
+//
+
+import XCTest
+
+class MVVMBeerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
